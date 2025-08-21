d517c7b7513767dc4d2c1b4c74a32fe62b908cb8
Emily Black <black.emily.nicole@gmail.com>
2025-08-21 16:13:48 -0700

    Adding function "assign lat lon"

---
+++ R/functions/assign_lat_lon.R
@@ -0,0 +1,49 @@
+#Function to assign latitude and longitude to madinley outputs 
+#Created on: Aug 21 2025
+#Created by ENB
+#Last edited: 
+#Last edited by: 
+
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+#Step 0. Download dependencies
+rm(list=ls())
+
+#load relevant libraries for script
+pkgs <- c("tidyverse", "terra", "MadingleyR")
+#install.packages(pkgs)
+lapply(pkgs, library, character.only = TRUE)
+rm(pkgs)
+
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_
+#Function
+#Requires: a madingley data input with spatial window, grid size, and 
+#Grid cell indices
+
+assign_lat_lon <- function(madingley_data) {
+  ncol <- (madingley_data$spatial_window[2] - madingley_data$spatial_window[1]) / madingley_data$grid_size
+  nrow <- (madingley_data$spatial_window[4] - madingley_data$spatial_window[3]) / madingley_data$grid_size
+  
+  lat_lon_output <-  rast(
+    nrows = nrow,
+    ncols = ncol,
+    xmin = madingley_data$spatial_window[1],
+    xmax = madingley_data$spatial_window[2],
+    ymin = madingley_data$spatial_window[3],
+    ymax = madingley_data$spatial_window[4],
+    crs = "EPSG:4326"  # lat/long
+  )
+  
+  coords <- as.data.frame(terra::xyFromCell(lat_lon_output, c(unique(madingley_data$cohorts$GridcellIndex) + 1)))
+  
+  coords_with_IDs <- cbind(coords, c(unique(madingley_data$cohorts$GridcellIndex)))
+  colnames(coords_with_IDs) <- c("lon", "lat", "GridcellIndex")
+  
+  madingley_data$cohorts <- dplyr::left_join(madingley_data$cohorts, coords_with_IDs, by = "GridcellIndex")
+  madingley_data$stocks <- dplyr::left_join(madingley_data$stocks, coords_with_IDs, by = "GridcellIndex")
+  
+  
+  return(madingley_data)
+}
+
