b21dd6f0e7c5eb911957ee8d7933f1f0cc997ec4
Gabriel Bergeron <gabriel.bergeron3@usherbrooke.ca>
2025-10-01 13:58:44 -0400

    updated layout + fake output
    Updated layout after talking with the simulation team.
    Created fake placeholder output files to play with the shiny app

--- app/madingley_shiny/ui.R
+++ app/madingley_shiny/ui.R
@@ -24,45 +24,118 @@ fluidPage(
     
     # Sidebar with input controls
     sidebarLayout(
-        tagAppendAttributes(sidebarPanel(
-            width = 3,
-            h4("Measure of Essential biodiversity variables using Madingley"),
-            
-            p(
-                "This is an area where we will breifly describe the project and what this app does. Exemple of content include : origin of the input data, how the model was computed, what are the avaible EBV and what do they means, etc.."
+        tagAppendAttributes(
+            sidebarPanel(
+                width = 3,
+                h4("Measure of Essential biodiversity variables using Madingley"),
+                
+                p(
+                    "This is an area where we will breifly describe the project and what this app does. Exemple of content include : origin of the input data, how the model was computed, what are the avaible EBV and what do they means, etc.."
+                ),
+                
+                h4("Input variables", style = "text-align:center;"),
+                
+                # Selection of the horizon of the climate projection
+                radioButtons(
+                    inputId = "temp",
+                    label = "Climate scenario",
+                    choices = c("Baseline" = "2020",
+                                "2050" = "2050"), 
+                    inline = TRUE
+                ),
+                
+                # Select the human impact projection
+                radioButtons(
+                    inputId = "HANPP",
+                    label = "Human footprint scenario",
+                    choices = c("Baseline" = "2020",
+                                "2050" = "2050"),
+                    inline = TRUE
+                ),
+                
+                h4("Special scenarios", style = "text-align:center;"),
+                
+                # Select one of two special scenarios
+                radioButtons(
+                    inputId = "scenario",
+                    label = "",
+                    choices = c("Baseline" = "Baseline",
+                                "Large predator removal" = "NoPred",
+                                "Large herbivore removal" = "NoHerb"),
+                    inline = FALSE
+                ),
             ),
-            
-            # Selection of the horizon of the projection
-            # CAUTION ***** Please note that the projection for 2040 did not exist so we copied 2045 *****
-            sliderInput(
-                inputId = "projection_year",
-                label = "Horizon of the projection",
-                min = 2025,
-                max = 2055,
-                value = 2025,
-                step = 5
-            ),
-        ), class = "sticky"),
+            class = "sticky"
+        ),
         
         mainPanel(
-            # width = 9,
-            
-            # Row 1: two cards side by side
-            fluidRow(
-                style = "display:flex;",
-                # Control card heigh
-                column(6, card(
-                    # Left plot, raster of land in 2020
-                    h3(textOutput("Land use - 2020")), 
-                    plotOutput("rasterHANPPpresent"), 
-                    style = "flex:1; height:35vh;"
-                )),
-                column(6, card(
-                    # Right plot, raster of l;and use on the selected horizon
-                    h3(textOutput("Land use - 2020")), 
-                    plotOutput("rasterHANPPfuture"), 
-                    style = "flex:1; height:35vh;"
-                ))
+
+            column(
+                width = 12, 
+                
+                # Title of the grid
+                h2("Input variables", style = "text-align:center; margin-bottom:20px;"),
+                
+                # Subheaders aligned with each card
+                fluidRow(
+                    column(4, h4("Initialisation", style = "text-align:center;")),
+                    column(4, h4("Simulation", style = "text-align:center;")),
+                    column(4, h4("Differences", style = "text-align:center;"))
+                ),
+                
+                
+                # === 3 columns × 2 rows grid of cards ===
+                layout_column_wrap(
+                    width = 1 / 3,
+                    # 3 card per row
+                    heigt = "35vh",
+                    
+                    # First row
+                    card(plotOutput("rasterinittemp", height = "250px")),
+                    card(plotOutput("rastersimtemp", height = "250px")),
+                    card(plotOutput("rasterdifftemp", height = "250px")),
+                    
+                    # Second row
+                    card(plotOutput("rasterinitHANPP", height = "250px")),
+                    card(plotOutput("rastersimHANPP", height = "250px")),
+                    card(plotOutput("rasterdiffHANPP", height = "250px")),
+                ),
+                
+                h2("Essential Biodiversity Variables", style = "text-align:center; margin-bottom:20px;"),
+                
+                h3("Shannon Diversity index", style = "text-align:center; margin-bottom:20px;"),
+                
+                # Subheaders aligned with each card
+                fluidRow(
+                    column(4, h4("Initialisation", style = "text-align:center;")),
+                    column(4, h4("Simulation", style = "text-align:center;")),
+                    column(4, h4("Differences", style = "text-align:center;"))
+                ),
+                
+                # === 3 columns × 1 rows grid of cards ===
+                layout_column_wrap(
+                    width = 1 / 3,
+                    # 3 card per row
+                    heigt = "35vh",
+                    
+                    # First row
+                    card(plotOutput("rasterinitshannon", height = "250px")),
+                    card(plotOutput("rastersimshannon", height = "250px")),
+                    card(plotOutput("rasterdiffshannon", height = "250px"))
+                ),
+                
+                h3("Seasonality of births", style = "text-align:center; margin-bottom:20px;"),
+                
+                # === 2 columns × 1 rows grid of cards ===
+                layout_column_wrap(
+                    width = 1 / 2,
+                    # 3 card per row
+                    heigt = "35vh",
+                    
+                    # First row
+                    card(plotOutput("seasonalityinit")),
+                    card(plotOutput("seasonalitysim"))
+                )
             )
         )
     )
